b6f42d99d43b7b7699310a485510b9a3f6f6307e
hisd3dev3 <rdquilicot14@gmail.com>
2025-07-15 13:53:03 +0800

    change url

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-	uri: "http://localhost:4000/graphql",
+	uri: "https://soi-backend.onrender.com/graphql",
 	credentials: "include",
 });
 
